0358d427b1118b151f130735fc535b47ad24296e
Rochan Madhusudhana <rochana.hm@gmail.com>
2023-11-13 23:33:17 -0500

    Update tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -6,20 +6,8 @@ load_dotenv()
 
 from sqlalchemy import create_engine, text
 
+from review_visualizer.db.models import Product
 from review_visualizer.db.session import get_db, init_db
-
-
-def test_db_connection():
-    USERNAME = os.getenv("USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@aws.connect.psdb.cloud/dva-project"
-
-    # Test that the database connection is working
-    engine = create_engine(DATABASE_URL)
-    connection = engine.connect()
-    query = text("SELECT 1;")
-    result = connection.execute(query)
-    assert result.scalar() == 1
 
 
 def test_init_db():
@@ -30,5 +18,7 @@ def test_init_db():
 def test_get_db():
     init_db()
     with get_db() as db:
+        prod = db.query(Product).first()
+        assert prod is not None
         assert db is not None
         assert db.is_active is True
