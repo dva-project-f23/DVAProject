3239b0a4ce3ff12c8d17704cc09ac53a058f4a97
Rochan Madhusudhana <rochana.hm@gmail.com>
2023-11-13 23:23:18 -0500

    Change schema

--- review_visualizer/db/models.py
+++ review_visualizer/db/models.py
@@ -1,21 +1,55 @@
-from sqlalchemy import JSON, Column, DateTime, Float, Integer, String, Text
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    asin = Column(String, unique=True)
+    title = Column(String)
+    price = Column(Float)
+    imUrl = Column(String)
+    primaryCategory = Column(String)  # Key of JSON field - Sales Rank
+    salesRank = Column(Integer)  # Value of JSON field - Sales Rank
+    brand = Column(String)
+
+    categories = relationship("Category", secondary="product_category_link")
+    reviews = relationship("Review", backref="product")
+    related_products = relationship("RelatedProduct", backref="product")
 
 
 class Review(Base):
     __tablename__ = "reviews"
 
-    reviewID = Column(Integer, primary_key=True, autoincrement=True)
-    reviewerID = Column(String(255), nullable=False)
-    asin = Column(String(255), nullable=False)
-    reviewerName = Column(Text)
-    helpful = Column(JSON)
+    id = Column(Integer, primary_key=True)
+    reviewerID = Column(String)
+    asin = Column(String, ForeignKey("products.asin"))
+    reviewerName = Column(String)
+    helpfulCount = Column(Integer)  # Number of people who found the review helpful
+    totalHelpfulCount = Column(
+        Integer
+    )  # Total number of people who rated the review for helpfulness
     reviewText = Column(Text)
     overall = Column(Float)
     summary = Column(Text)
     unixReviewTime = Column(DateTime)
 
-    def __repr__(self):
-        return self.reviewerName
+
+class RelatedProduct(Base):
+    __tablename__ = "related_products"
+
+    id = Column(Integer, primary_key=True)
+    asin = Column(String, ForeignKey("products.asin"))
+    related_asin = Column(String)  # ASIN of the related product
+    relation_type = Column(String)  # e.g., also_bought, also_viewed, etc.
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
