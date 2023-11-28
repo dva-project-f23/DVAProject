8754d555be8d271f1576979c4073b0669b82468e
Rochan Madhusudhana <rochana.hm@gmail.com>
2023-11-28 13:17:26 -0500

    Fix sentiment analysis

--- review_visualizer/app/pages/overview.py
+++ review_visualizer/app/pages/overview.py
@@ -1,54 +1,47 @@
 import asyncio
 import html
+from datetime import datetime
 from typing import Any, List
-import pandas as pd
 
+import pandas as pd
 import streamlit as st
+from dateutil import relativedelta
 from st_pages import add_page_title
 from streamlit_searchbox import st_searchbox
 
+from moving_average import ma_average
 from review_visualizer.db.prisma import PrismaClient
 from review_visualizer.visualizations.graphs import example_graph
-from moving_average import ma_average
-from datetime import datetime
-from dateutil import relativedelta
 
 add_page_title("Overview")
 
 from dataclasses import dataclass
+
+from prisma.models import Product
 
 
 @dataclass
 class SearchResults:
     asin: str
     title: str
-    price: float
-    brand: str
+    product: Product
 
     def __repr__(self):
         return html.unescape(self.title)
 
 
-async def search_results(query: str) -> List[Any]:
+async def search_results(query: str) -> List[SearchResults]:
     # Use database to search for query
     async with PrismaClient() as prisma:
         results = await prisma.product.find_many(
             where={
-                'AND': [
+                "AND": [
                     {
                         "title": {
                             "contains": query,
                         },
                     },
-                    {
-                        "reviews": {
-                            "some": {
-                                "overall": {
-                                    'gte': 0.0
-                                }
-                            }
-                        }
-                    }
+                    {"reviews": {"some": {"overall": {"gte": 0.0}}}},
                 ]
             },
             take=5,
@@ -58,41 +51,60 @@ async def search_results(query: str) -> List[Any]:
         SearchResults(
             asin=result.asin,
             title=result.title,
-            price=result.price,
-            brand=result.brand,
+            product=result,
         )
         for result in results
     ]
 
 
-def search_results_sync(query: str) -> List[Any]:
+def search_results_sync(query: str) -> List[SearchResults]:
     return asyncio.run(search_results(query))
 
 
 # Search bar
-product = st_searchbox(
+search_res: SearchResults = st_searchbox(
     search_results_sync,
     placeholder="Search for a product",
     key="product_search",
 )
 
-if product:
-    st.write("Amazon Standard Identification Number: " + product.asin)
-    if (product.brand == None):
-        st.write("Brand: n/a")
-    else:
-        st.write("Brand: " + product.brand)
-    if (product.price == None):
-        st.write("Price: n/a")
-    else:
-        st.write("Price: " + str(round(product.price, 2)) + " $")
-    df, total_reviews = asyncio.run(ma_average.movingavg(product.asin))
-    choice_date = df["Date"][0].strftime('%Y-%m')
+if search_res:
+    st.subheader("Product Information")
+    st.write("**Amazon Standard Identification Number:** " + search_res.asin)
+    st.write(
+        f"**Brand:** {search_res.product.brand}" if search_res.product.brand else ""
+    )
+    st.write(
+        f"**Price:** ${search_res.product.price:.2f}"
+        if search_res.product.price
+        else ""
+    )
+    st.write(
+        f"**Product Category:** {search_res.product.primaryCategory}"
+        if search_res.product.primaryCategory
+        else ""
+    )
+    st.write(
+        f"**Product Sales Rank:** {search_res.product.salesRank}"
+        if search_res.product.salesRank
+        else ""
+    )
+
+    if search_res.product.imUrl:
+        st.image(search_res.product.imUrl, width=300)
+
+    st.subheader("Product Review Analysis")
+    df, total_reviews = asyncio.run(ma_average.movingavg(search_res.asin))
+    choice_date = df["Date"][0].strftime("%Y-%m")
     earliest = df["Date"][0]
-    choice_date = st.selectbox("Graph starting month", [d.strftime('%Y-%m') for d in df["Date"]])
+    choice_date = st.selectbox(
+        "Graph starting month", [d.strftime("%Y-%m") for d in df["Date"]]
+    )
     if choice_date:
-        curr = datetime.strptime(choice_date, '%Y-%m')
+        curr = datetime.strptime(choice_date, "%Y-%m")
         diff = relativedelta.relativedelta(curr, earliest)
-        fig = ma_average.make_graph(df[(diff.months + 12*diff.years):], total_reviews)
+        fig = ma_average.make_graph(
+            df[(diff.months + 12 * diff.years) :], total_reviews
+        )
         fig.update_layout(height=600)
-        st.plotly_chart(fig,use_container_width=True,height=600)
+        st.plotly_chart(fig, use_container_width=True, height=600)
