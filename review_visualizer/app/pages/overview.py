dcbbf25915aa85ff7d88c7c604c4e57aa01bfd55
Parisa Babolhavaeji <pbabolhavaeji3@gatech.edu>
2023-12-02 15:19:41 -0800

    similar product

--- review_visualizer/app/pages/overview.py
+++ review_visualizer/app/pages/overview.py
@@ -55,6 +55,17 @@ async def search_results(query: str) -> List[SearchResults]:
         for result in results
     ]
 
+async def get_similar_products(asin: str) -> List[Product]:
+    async with PrismaClient() as prisma:
+        similar_products_asins = await prisma.product.find_unique(
+            where={"asin": asin},
+            select={"similar": True}
+        )
+        if similar_products_asins and similar_products_asins.similar:
+            return await prisma.product.find_many(
+                where={"asin": {"in": similar_products_asins.similar}}
+            )
+        return []
 
 async def get_review_stats(asin: str) -> dict:
     async with PrismaClient() as prisma:
@@ -81,6 +92,67 @@ async def get_review_stats(asin: str) -> dict:
 def search_results_sync(query: str) -> List[SearchResults]:
     return asyncio.run(search_results(query))
 
+def create_network_graph(center_product, similar_products, average_ratings):
+    edge_x = []
+    edge_y = []
+    node_x = []
+    node_y = []
+    node_text = []
+
+    # Center node
+    node_x.append(0)
+    node_y.append(0)
+    node_text.append(center_product.title)
+
+    # Similar nodes
+    for i, product in enumerate(similar_products):
+        angle = (2 * 3.14159 / len(similar_products)) * i
+        x, y = np.cos(angle), np.sin(angle)
+        node_x.append(x)
+        node_y.append(y)
+        node_text.append(product.title)
+
+        edge_x.extend([0, x, None])
+        edge_y.extend([0, y, None])
+
+    # Adjust node size based on average rating
+    max_rating = max(average_ratings.values())
+    node_size = [10 + 20 * (average_ratings[product.asin] / max_rating) if average_ratings[product.asin] else 10 for product in similar_products]
+    
+    # Adjust node color based on price
+    max_price = max(product.price for product in similar_products if product.price)
+    node_color = [np.interp(product.price, [0, max_price], [0.5, 1]) if product.price else 0.5 for product in similar_products]
+    
+    # Create Plotly trace for edges
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    # Create Plotly trace for nodes
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers+text',
+        text=node_text,
+        marker=dict(
+            showscale=False,
+            color=['rgba(138, 43, 226, {})'.format(shade) for shade in node_color],
+            size=node_size,
+            line_width=2))
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        showlegend=False,
+                        hovermode='closest',
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    return fig
+
+def search_results_sync(query: str) -> List[SearchResults]:
+    return asyncio.run(search_results(query))
 
 # Search bar
 search_res: SearchResults = st_searchbox(
@@ -163,3 +235,25 @@ if search_res:
 
         st.plotly_chart(fig1, use_container_width=True)
         st.plotly_chart(fig2, use_container_width=True)
+
+    st.subheader("Similar Products")
+    similar_products = asyncio.run(get_similar_products(search_res.asin))
+    if similar_products:
+        # Compute average ratings for similar products
+        similar_products_asins = [product.asin for product in similar_products]
+        average_ratings = asyncio.run(get_average_ratings(similar_products_asins))
+
+        # Display a table of similar products with their prices
+        similar_products_data = {
+            "Title": [product.title for product in similar products],
+            "Price": [product.price for product in similar products],
+            "Average Rating": [average_ratings[asin] for asin in similar_products_asins]
+        }
+        similar_products_df = pd.DataFrame(similar_products_data)
+        st.table(similar_products_df.head(3))  # Display top 3 similar products
+
+        # Create and display the network graph
+        network_graph = create_network_graph(search_res.product, similar_products, average_ratings)
+        st.plotly_chart(network_graph, use_container_width=True)
+    else:
+        st.write("No similar products found.")
