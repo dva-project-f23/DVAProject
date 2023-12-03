26d39d76566dd49335f633dbb0bf5827a12be162
Shayar Shah <shahshayar@gmail.com>
2023-12-02 19:11:25 -0500

    Update About.py

--- review_visualizer/app/pages/About.py
+++ review_visualizer/app/pages/About.py
@@ -1,10 +1,22 @@
 import os
 import streamlit as st
+from docx import Document
 
 st.set_page_config(layout="wide")
 
-
 st.title("About")
 
-with open(os.path.join(os.path.dirname(__file__), "About.docx"), "r") as f:
-    st.markdown(f.read())
+# Path to your .docx file
+docx_file_path = os.path.join(os.path.dirname(__file__), "About.docx")
+
+# Function to read .docx file
+def read_docx(file_path):
+    doc = Document(file_path)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
+
+# Display the contents of the .docx file
+doc_content = read_docx(docx_file_path)
+st.write(doc_content)
